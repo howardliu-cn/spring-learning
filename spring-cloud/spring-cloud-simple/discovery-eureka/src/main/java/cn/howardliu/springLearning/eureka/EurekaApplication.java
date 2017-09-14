fbf5140e136a4639490cc1c113f79ac447e817f8
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-14 14:27:45 +0800

    discovery-eureka

---
+++ spring-cloud/spring-cloud-simple/discovery-eureka/src/main/java/cn/howardliu/springLearning/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package cn.howardliu.springLearning.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * <br>created at 17-9-14
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
