99c83c546d144b684b214d237444d79f1a02eef3
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-14 15:39:47 +0800

    consume use ribbon from eureka

--- spring-cloud/spring-cloud-simple/simple-consumer-movie/src/main/java/cn/howardliu/springLearning/simple/controller/MovieController.java
+++ spring-cloud/spring-cloud-simple/simple-consumer-movie/src/main/java/cn/howardliu/springLearning/simple/controller/MovieController.java
@@ -2,6 +2,8 @@ package cn.howardliu.springLearning.simple.controller;
 
 import cn.howardliu.springLearning.simple.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +18,21 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class MovieController {
     private final RestTemplate restTemplate;
+    private final LoadBalancerClient loadBalancerClient;
 
     @Autowired
-    public MovieController(RestTemplate restTemplate) {
+    public MovieController(RestTemplate restTemplate, LoadBalancerClient loadBalancerClient) {
         this.restTemplate = restTemplate;
+        this.loadBalancerClient = loadBalancerClient;
     }
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
-        return restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+        return restTemplate.getForObject("http://simple-provider-user/" + id, User.class);
+    }
+
+    @GetMapping("/log-instance")
+    public ServiceInstance logUserInstance() {
+        return this.loadBalancerClient.choose("simple-provider-user");
     }
 }
