36d9aad7ce4f52de6e7c1c9f5350d2890ba7690b
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-14 14:52:02 +0800

    regist simple-consumer-movie to eureka

--- spring-cloud/spring-cloud-simple/simple-consumer-movie/src/main/java/cn/howardliu/springLearning/simple/ConsumerMovieApplication.java
+++ spring-cloud/spring-cloud-simple/simple-consumer-movie/src/main/java/cn/howardliu/springLearning/simple/ConsumerMovieApplication.java
@@ -2,6 +2,7 @@ package cn.howardliu.springLearning.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
  * @since 1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerMovieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMovieApplication.class, args);
