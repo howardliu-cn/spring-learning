faeee17392b033d24c84bab463eb17fb89806173
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-14 14:48:08 +0800

    regist simple-provider-user to eureka

--- spring-cloud/spring-cloud-simple/simple-provider-user/src/main/java/cn/howardliu/springLearning/simple/ProviderUserApplication.java
+++ spring-cloud/spring-cloud-simple/simple-provider-user/src/main/java/cn/howardliu/springLearning/simple/ProviderUserApplication.java
@@ -2,6 +2,7 @@ package cn.howardliu.springLearning.simple;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * <br>created at 17-9-14
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderUserApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProviderUserApplication.class, args);
