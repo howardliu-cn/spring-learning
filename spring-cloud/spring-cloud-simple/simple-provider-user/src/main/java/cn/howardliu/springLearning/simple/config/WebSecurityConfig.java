5023890b02a2dab571128fb0e591e5984fad7a6a
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-15 14:49:54 +0800

    provide user use security

---
+++ spring-cloud/spring-cloud-simple/simple-provider-user/src/main/java/cn/howardliu/springLearning/simple/config/WebSecurityConfig.java
@@ -0,0 +1,138 @@
+package cn.howardliu.springLearning.simple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * <br>created at 17-9-15
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Component
+    class CustomUserDetailsService implements UserDetailsService {
+        @Override
+        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+            if ("user".equals(username)) {
+                return new SecurityUser("user", "password1", "user-role");
+            } else if ("admin".equals(username)) {
+                return new SecurityUser("admin", "password2", "admin-role");
+            }
+            return null;
+        }
+    }
+
+    class SecurityUser implements UserDetails {
+        private Long id;
+        private String username;
+        private String password;
+        private String role;
+
+        public SecurityUser() {
+        }
+
+        public SecurityUser(String username, String password, String role) {
+            super();
+            this.username = username;
+            this.password = password;
+            this.role = role;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority(role);
+            authorities.add(authority);
+            return authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getRole() {
+            return role;
+        }
+
+        public void setRole(String role) {
+            this.role = role;
+        }
+    }
+}
--- spring-cloud/spring-cloud-simple/simple-provider-user/src/main/java/cn/howardliu/springLearning/simple/controller/UserController.java
+++ spring-cloud/spring-cloud-simple/simple-provider-user/src/main/java/cn/howardliu/springLearning/simple/controller/UserController.java
@@ -2,10 +2,17 @@ package cn.howardliu.springLearning.simple.controller;
 
 import cn.howardliu.springLearning.simple.dao.UserRepository;
 import cn.howardliu.springLearning.simple.pojo.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 /**
  * <br>created at 17-9-14
@@ -15,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserRepository userRepository;
 
     @Autowired
@@ -24,6 +32,14 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User findById(@PathVariable Long id) {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            UserDetails user = (UserDetails) principal;
+            Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+            for (GrantedAuthority authority : authorities) {
+                logger.info("username is {}, role is {}", user.getUsername(), authority.getAuthority());
+            }
+        }
         return this.userRepository.findOne(id);
     }
 }
