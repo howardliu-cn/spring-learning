bde3cb7841905e46bd466aab397c496153df4788
howardliu-cn <liuxinghao1988@gmail.com>
2017-09-18 14:45:09 +0800

    hystrix stream dashboard

--- spring-cloud/spring-cloud-simple/consumer-movie-feign-hystrix-fallback-stream/src/main/java/cn/howardliu/springLearning/simple/ConsumerMovieApplication.java
+++ spring-cloud/spring-cloud-simple/consumer-movie-feign-hystrix-fallback-stream/src/main/java/cn/howardliu/springLearning/simple/ConsumerMovieApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -19,6 +20,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ConsumerMovieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerMovieApplication.class, args);
