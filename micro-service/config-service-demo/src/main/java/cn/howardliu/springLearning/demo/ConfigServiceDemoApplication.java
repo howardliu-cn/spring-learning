371d073ed4496880eb904c3f681a421e71c29a97
howardliu-cn <liuxinghao1988@gmail.com>
2017-10-09 13:02:40 +0800

    config-service-demo module

---
+++ micro-service/config-service-demo/src/main/java/cn/howardliu/springLearning/demo/ConfigServiceDemoApplication.java
@@ -0,0 +1,17 @@
+package cn.howardliu.springLearning.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <br>created at 17-10-9
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ConfigServiceDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServiceDemoApplication.class, args);
+    }
+}
---
+++ micro-service/config-service-demo/src/main/java/cn/howardliu/springLearning/demo/controller/ValueController.java
@@ -0,0 +1,28 @@
+package cn.howardliu.springLearning.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <br>created at 17-10-9
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+@RestController
+public class ValueController {
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("from")
+    public String from() {
+        return env.getProperty("from", "undefined");
+    }
+
+    @RequestMapping("to")
+    public String to() {
+        return env.getProperty("to", "undefined");
+    }
+}
