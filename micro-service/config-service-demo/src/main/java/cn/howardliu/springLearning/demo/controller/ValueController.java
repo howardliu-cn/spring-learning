fc2c7bed233cb1d3b2aaed68a1d9748b73907938
howardliu-cn <liuxinghao1988@gmail.com>
2017-10-12 09:11:12 +0800

    use an asymmetric key

--- micro-service/config-service-demo/src/main/java/cn/howardliu/springLearning/demo/controller/ValueController.java
+++ micro-service/config-service-demo/src/main/java/cn/howardliu/springLearning/demo/controller/ValueController.java
@@ -2,6 +2,7 @@ package cn.howardliu.springLearning.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class ValueController {
     @RequestMapping("to")
     public String to() {
         return env.getProperty("to", "undefined");
+    }
+
+    @RequestMapping("value/{v}")
+    public String value(@PathVariable("v") String v) {
+        return env.getProperty(v, "undefined");
     }
 }
