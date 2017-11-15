ba42740979cb6571077c6ae2dddd548025825afc
howardliu-cn <liuxinghao1988@gmail.com>
2017-11-15 21:55:15 +0800

    file upload demo

---
+++ spring-mvc-jsonp/src/main/java/cn/howardliu/spring/mvc/jsonp/controller/FileUploadController.java
@@ -0,0 +1,42 @@
+package cn.howardliu.spring.mvc.jsonp.controller;
+
+import cn.howardliu.spring.mvc.jsonp.pojo.User;
+import net.sf.json.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * <br>created at 17-11-15
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+@Controller
+@RequestMapping("file")
+public class FileUploadController {
+    @RequestMapping("upload")
+    public JSONObject upload(User user, @RequestParam(value = "files", required = false) MultipartFile[] files) {
+        JSONObject json = new JSONObject();
+        try {
+            for (MultipartFile file : files) {
+                System.out.println(file.getName());
+                System.out.println(file.getContentType());
+                System.out.println(file.getOriginalFilename());
+                IOUtils.copy(file.getInputStream(),
+                        new FileOutputStream(new File("/home/liuxh/" + file.getOriginalFilename())));
+            }
+            json.put("success", true);
+        } catch (Exception e) {
+            json.put("success", false);
+        }
+        json.put("length", files.length);
+        json.put("user", user == null ? "null" : user);
+        return json;
+    }
+}
---
+++ spring-mvc-jsonp/src/main/java/cn/howardliu/spring/mvc/jsonp/pojo/User.java
@@ -0,0 +1,28 @@
+package cn.howardliu.spring.mvc.jsonp.pojo;
+
+/**
+ * <br>created at 17-11-15
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+public class User {
+    private String username;
+    private String nickname;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
