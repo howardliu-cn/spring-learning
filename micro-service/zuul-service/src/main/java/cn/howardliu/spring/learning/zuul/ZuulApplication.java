69c03cae894a69caa2de699bb41ede944c175de7
howardliu-cn <liuxinghao1988@gmail.com>
2018-03-02 11:06:45 +0800

    zuul service

---
+++ micro-service/zuul-service/src/main/java/cn/howardliu/spring/learning/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package cn.howardliu.spring.learning.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * <br>created at 18-3-2
+ *
+ * @author liuxh
+ * @since 0.1.0
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
