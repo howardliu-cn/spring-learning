61400e76432e68a1e1c52d6a1527dfed994c1224
howardliu-cn <liuxinghao1988@gmail.com>
2018-03-06 15:18:25 +0800

    jsonp

---
+++ spring-mvc-jsonp/src/main/java/cn/howardliu/spring/mvc/jsonp/FileUploadMain.java
@@ -0,0 +1,33 @@
+package cn.howardliu.spring.mvc.jsonp;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.MultipartRequestEntity;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.params.HttpClientParams;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * <br>created at 17-11-15
+ *
+ * @author liuxh
+ * @since 1.0.0
+ */
+public class FileUploadMain {
+    public static void main(String[] args) throws IOException {
+        HttpClient httpClient = new HttpClient();
+        PostMethod postMethod = new PostMethod("http://localhost:8080/spring-mvc-jsonp/file/upload");
+        postMethod.addParameter("username", "abc");
+        postMethod.addParameter("nickname", "def");
+        Part[] parts = {new FilePart("favicon.png", new File("/home/liuxh/Pictures/favicon.png"))};
+        postMethod.setRequestEntity(new MultipartRequestEntity(parts, postMethod.getParams()));
+        httpClient.getHttpConnectionManager().getParams().setConnectionTimeout(5000);
+        int status = httpClient.executeMethod(postMethod);
+        System.out.println(status);
+        System.out.println(postMethod.getResponseBodyAsString());
+        postMethod.releaseConnection();
+    }
+}
