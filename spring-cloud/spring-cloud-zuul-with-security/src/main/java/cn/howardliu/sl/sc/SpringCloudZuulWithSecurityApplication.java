a1540f430de1109d138aa2b5d0e7b6aa83439aef
howardliu-cn <howardliu1988@163.com>
2018-08-05 15:25:28 +0800

    ZuulDebugFilter

--- spring-cloud/spring-cloud-zuul-with-security/src/main/java/cn/howardliu/sl/sc/SpringCloudZuulWithSecurityApplication.java
+++ spring-cloud/spring-cloud-zuul-with-security/src/main/java/cn/howardliu/sl/sc/SpringCloudZuulWithSecurityApplication.java
@@ -1,9 +1,16 @@
 package cn.howardliu.sl.sc;
 
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.List;
 
 /**
  * <br>created at 18-7-17
@@ -17,5 +24,62 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 public class SpringCloudZuulWithSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudZuulWithSecurityApplication.class, args);
+    }
+
+    @Bean
+    public ZuulFilter routingDebugFilter() {
+        return new ZuulFilter() {
+            @Override
+            public String filterType() {
+                return "post";
+            }
+
+            @Override
+            public int filterOrder() {
+                return 999999;
+            }
+
+            @Override
+            public boolean shouldFilter() {
+                return true;
+            }
+
+            @Override
+            public Object run() {
+                final List<String> routingDebug = (List<String>) RequestContext.getCurrentContext().get("routingDebug");
+                routingDebug.forEach(System.out::println);
+                return null;
+            }
+        };
+    }
+
+    @Bean
+    public ZuulFilter authHeaderFilter() {
+        return new ZuulFilter() {
+            @Override
+            public String filterType() {
+                return "pre";
+            }
+
+            @Override
+            public int filterOrder() {
+                return 0;
+            }
+
+            @Override
+            public boolean shouldFilter() {
+                return true;
+            }
+
+            @Override
+            public Object run() {
+                Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication != null && authentication.getPrincipal() != null) {
+                    RequestContext requestContext = RequestContext.getCurrentContext();
+                    requestContext.addZuulRequestHeader("X-AUTH-ID", authentication.getPrincipal().toString());
+                }
+                return null;
+            }
+        };
     }
 }
